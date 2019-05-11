57b353c7ba0cc3347a4cc4874d692c026f577f65
gil <gil.zhang.dev@outlook.com>
2019-05-11 13:50:08 +0800

    进行串口读取的界面优化

--- WpfWcf/Control.cs
+++ WpfWcf/Control.cs
@@ -11,21 +11,21 @@ namespace WpfWcf
     // 注意: 使用“重构”菜单上的“重命名”命令，可以同时更改代码和配置文件中的类名“Control”。
     public class Control : IControl
     {
-        SerialPort myPort = null;
+        //SerialPort myPort = null;
         public string DoWork(string filename)
         {
-            if(myPort==null)
-            {
-                myPort = new SerialPort();
-                myPort.BaudRate = 9600;
-                myPort.DataBits = 8;
-                myPort.PortName = "COM6";
-                myPort.NewLine = "\r\n";
-                myPort.Open();
-            }
-             
-            string res = myPort.ReadLine();
-            myPort.Close();
+            //if(myPort==null)
+            //{
+            //    myPort = new SerialPort();
+            //    myPort.BaudRate = 9600;
+            //    myPort.DataBits = 8;
+            //    myPort.PortName = "COM6";
+            //    myPort.NewLine = "\r\n";
+            //    myPort.Open();
+            //}
+            string res=  MainWindow.Instance.myPort.ReadLine();
+             //= myPort.ReadLine();
+           // myPort.Close();
             //myPort.DataReceived += MyPort_DataReceived;
             return res;
         }
--- WpfWcf/MainWindow.xaml.cs
+++ WpfWcf/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Ports;
 using System.Linq;
 using System.ServiceModel;
 using System.ServiceModel.Description;
@@ -22,27 +23,45 @@ namespace WpfWcf
     /// </summary>
     public partial class MainWindow : Window
     {
+      public static MainWindow  Instance=null;
         public MainWindow()
         {
             InitializeComponent();
+            Instance = this;
+            
         }
-       
+      public  SerialPort myPort = null;
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-
+ 
             string baseAddress = "http://" + Environment.MachineName + ":8000/Service";
             ServiceHost host = new ServiceHost(typeof(WpfWcf.Control), new Uri(baseAddress));
             host.AddServiceEndpoint(typeof(WpfWcf.IControl), new WebHttpBinding(), "").Behaviors.Add(new WebHttpBehavior());
             host.Open();
+            if (myPort == null)
+            {
+                myPort = new SerialPort();
+                myPort.BaudRate = 9600;
+                myPort.DataBits = 8;
+                myPort.PortName = Com.Text;
+                myPort.NewLine = "\r\n";
+                myPort.Open();
+            }
 
         }
 
         private void Window_Closed(object sender, EventArgs e)
         {
-
-           
+            if(myPort!=null)
+            {
+                myPort.Close();
+            }          
         }
-   
-          
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            string[] ArryPort = SerialPort.GetPortNames();
+            Com.ItemsSource = ArryPort;
+        }
     }
 }
