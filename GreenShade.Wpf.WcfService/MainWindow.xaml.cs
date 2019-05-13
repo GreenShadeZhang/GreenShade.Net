8f5e919ddc376328e08ed17cadcea25a5f0ce676
gil <gil.zhang.dev@outlook.com>
2019-05-13 21:45:43 +0800

    添加生成二维码

--- GreenShade.Wpf.WcfService/MainWindow.xaml.cs
+++ GreenShade.Wpf.WcfService/MainWindow.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.IO.Ports;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.ServiceModel;
 using System.ServiceModel.Description;
 using System.Text;
@@ -15,7 +19,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using ZXing;
+using ZXing.Common;
 namespace GreenShade.Wpf.WcfService
 {
     /// <summary>
@@ -23,7 +28,7 @@ namespace GreenShade.Wpf.WcfService
     /// </summary>
     public partial class MainWindow : Window
     {
-        public static MainWindow Instance = null;
+        public static MainWindow Instance { get; set; } = null;
         public MainWindow()
         {
             InitializeComponent();
@@ -35,7 +40,7 @@ namespace GreenShade.Wpf.WcfService
         private void Button_Click(object sender, RoutedEventArgs e)
         {
 
-            if (host == null&&myPort == null && !string.IsNullOrEmpty(Com.Text))
+            if (host == null && myPort == null && !string.IsNullOrEmpty(Com.Text))
             {
                 try
                 {
@@ -82,6 +87,72 @@ namespace GreenShade.Wpf.WcfService
         {
             string[] ArryPort = SerialPort.GetPortNames();
             Com.ItemsSource = ArryPort;
+        }
+
+        private void QrcodeBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            QrCodeImg.Source = null;
+            try
+            {
+                string ipAdress = null;
+                string name = Dns.GetHostName();
+                IPAddress[] ipadrlist = Dns.GetHostAddresses(name);
+                foreach (IPAddress ipa in ipadrlist)
+                {
+                    if (ipa.AddressFamily == AddressFamily.InterNetwork)
+                    {
+                        if (ipa.ToString().StartsWith("192.168.1"))
+                        {
+                            ipAdress = "http://" + ipa.ToString() + ":8000/Service/Files?aid=123";
+                        }
+                    }
+                    // Console.Writeline(ipa.ToString());
+                }
+                IpUrl.Text = ipAdress;
+                GeneratorQRCode(ipAdress);
+
+            }
+            catch (Exception ex)
+            {
+                IpUrl.Text = ex.Message;//异常
+            }
+
+
+        }
+
+        // 二维码生成函数    
+        private System.Drawing.Image GeneratorQRCode(string txt)
+        {
+            //BarcodeWriter一个智能类来编码一些内容的二维码、条形码图像 
+            BarcodeWriter writer = new BarcodeWriter
+            {
+                Format = BarcodeFormat.QR_CODE//设置二维码的格式
+            };
+            writer.Options.Hints.Add(EncodeHintType.CHARACTER_SET, "UTF-8"); // 编码格式    
+            writer.Options.Hints.Add(EncodeHintType.ERROR_CORRECTION, ZXing.QrCode.Internal.ErrorCorrectionLevel.H);
+            int codeSizeInPixels = 260;      // 二维码长宽       
+            writer.Options.Height = codeSizeInPixels;
+            writer.Options.Width = codeSizeInPixels;
+            writer.Options.Margin = 0;       // 设置边框         
+            BitMatrix bm = writer.Encode(txt);
+            Bitmap img = writer.Write(bm);//对指定内容进行编码，并返回该码的呈现实例(渲染属性、实例使用，在方法之前调用)
+            QrCodeImg.Source = BitmapToBitmapImage(img);//将图片控件的数据源设为生成后的二维码
+            return img;
+        }
+        // Bitmap --> BitmapImage       
+
+        public static BitmapImage BitmapToBitmapImage(Bitmap bitmap)
+        {
+            using (MemoryStream stream = new MemoryStream())
+            {
+                bitmap.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                stream.Position = 0; BitmapImage result = new BitmapImage();
+                result.BeginInit(); result.CacheOption = BitmapCacheOption.OnLoad; result.StreamSource = stream; result.EndInit();
+                result.Freeze();
+                return result;
+            }
         }
     }
 }
