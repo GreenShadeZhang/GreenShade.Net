d40df58d15c14d7482fdb629fa98d66a3879ba77
gil <gil.zhang.dev@outlook.com>
2019-05-18 22:58:09 +0800

    添加小娜语音控制

--- GreenShade.UWP.RT.VoiceCommandService/ContanaVoiceCommandService.cs
+++ GreenShade.UWP.RT.VoiceCommandService/ContanaVoiceCommandService.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Windows.ApplicationModel.AppService;
 using Windows.ApplicationModel.Background;
 using Windows.ApplicationModel.VoiceCommands;
+using Windows.Web.Http;
 
 namespace GreenShade.UWP.RT.VoiceCommandService
 {
@@ -15,7 +16,8 @@ namespace GreenShade.UWP.RT.VoiceCommandService
         VoiceCommandServiceConnection voiceServiceConnection;
         public async void Run(IBackgroundTaskInstance taskInstance)
         {
-            serviceDeferral = taskInstance.GetDeferral();
+            string url = "http://localhost:8000/Service/controller?btn_id={0}";
+                  serviceDeferral = taskInstance.GetDeferral();
             taskInstance.Canceled += OnTaskCanceled;
             var triggerDetails = taskInstance.TriggerDetails as AppServiceTriggerDetails;
             if (triggerDetails != null && triggerDetails.Name == "ContanaVoiceCommandService")
@@ -25,26 +27,54 @@ namespace GreenShade.UWP.RT.VoiceCommandService
                     voiceServiceConnection = VoiceCommandServiceConnection.FromAppServiceTriggerDetails(triggerDetails);
                     voiceServiceConnection.VoiceCommandCompleted += OnVoiceCommandCompleted;
                     VoiceCommand voiceCommand = await voiceServiceConnection.GetVoiceCommandAsync();
+                    var userMessage = new VoiceCommandUserMessage();
+                    string returnMessage = null;                   
                     switch (voiceCommand.CommandName)
                     {
                         case "dosomething":
 
-                            var userMessage = new VoiceCommandUserMessage();
+                           
                             //var destinationsContentTiles = new List<VoiceCommandContentTile>();
                             //var destinationTile = new VoiceCommandContentTile();
 
                             //// To handle UI scaling, Cortana automatically looks up files with FileName.scale-<n>.ext formats based on the requested filename.
                             //// See the VoiceCommandService\Images folder for an example.
                             //destinationTile.ContentTileType = VoiceCommandContentTileType.TitleWith68x68IconAndText;
-
-
-                            //destinationTile.AppLaunchArgument = "牛逼";
-                            //destinationTile.Title = "傻瓜";
-                            userMessage.DisplayMessage = "牛逼";
-                            userMessage.SpokenMessage = "牛逼了你 我就不开灯";
-                           // destinationsContentTiles.Add(destinationTile);
+                            string reqUrl = string.Format(url,"circle");
+                            using (HttpClient httpClient = new HttpClient())
+                            {
+                                returnMessage= await httpClient.GetStringAsync(new Uri(reqUrl));
+                            }
+                                //destinationTile.AppLaunchArgument = "牛逼";
+                                //destinationTile.Title = "傻瓜";
+                                userMessage.DisplayMessage = returnMessage;
+                            userMessage.SpokenMessage = "好的 我在转圈圈";
+                            // destinationsContentTiles.Add(destinationTile);
                             var response = VoiceCommandResponse.CreateResponse(userMessage);
                             await voiceServiceConnection.ReportSuccessAsync(response);
+                            break;
+                        case "close":
+
+                           
+                            //var destinationsContentTiles = new List<VoiceCommandContentTile>();
+                            //var destinationTile = new VoiceCommandContentTile();
+
+                            //// To handle UI scaling, Cortana automatically looks up files with FileName.scale-<n>.ext formats based on the requested filename.
+                            //// See the VoiceCommandService\Images folder for an example.
+                            //destinationTile.ContentTileType = VoiceCommandContentTileType.TitleWith68x68IconAndText;
+                            string reqClose = string.Format(url, "stop");
+                            
+                            using (HttpClient httpClient = new HttpClient())
+                            {
+                                returnMessage = await httpClient.GetStringAsync(new Uri(reqClose));
+                            }
+                            //destinationTile.AppLaunchArgument = "牛逼";
+                            //destinationTile.Title = "傻瓜";
+                            userMessage.DisplayMessage = returnMessage;
+                            userMessage.SpokenMessage = "好的 停止了";
+                            // destinationsContentTiles.Add(destinationTile);
+                            var responseC = VoiceCommandResponse.CreateResponse(userMessage);
+                            await voiceServiceConnection.ReportSuccessAsync(responseC);
                             break;
                     }
 
